f3c6b1d2b67910731682f4a45669ba11eb3171b2
Samiksha Jain <samikshaj7371@gmail.com>
2025-12-12 22:06:09 +0530

    Initial commit

---
+++ src/auth.ts
@@ -0,0 +1,47 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import connectDb from "./lib/db";
+import User from "./models/user.model";
+import bcrypt from "bcryptjs";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      credentials: {
+        email: { label: "email", type: "email" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials, request) {
+        await connectDb();
+        const email = credentials.email;
+        const password = credentials.password as string;
+        const user = await User.findOne({ email });
+        if (!user) {
+          throw new Error("user does not exist");
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+          throw new Error("Incorrect password");
+        }
+        return {
+          id: user._id,
+          email: user.email,
+          name: user.name,
+          role: user.role,
+        };
+      },
+    }),
+  ],
+  callbacks: {
+    //token k andar user ka data dalta hai
+    jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.name = user.name;
+        token.email = user.email;
+        token.role = user.role;
+      }
+      return token;
+    },
+  },
+});
