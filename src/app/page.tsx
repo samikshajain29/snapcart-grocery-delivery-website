815f94e2dbb569d64f9303916803ccf00696b0f4
Samiksha Jain <samikshaj7371@gmail.com>
2026-01-12 20:54:42 +0530

    create edit role and mobile page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+import { auth } from "@/auth";
+import EditRoleMobile from "@/components/EditRoleMobile";
+import connectDb from "@/lib/db";
+import User from "@/models/user.model";
+import { redirect } from "next/navigation";
 import React from "react";
 
-function page() {
+async function Home() {
+  await connectDb();
+  const session = await auth();
+  const user = await User.findById(session?.user?.id);
+  if (!user) {
+    redirect("/login");
+  }
+
+  const incomplete =
+    !user.role || !user.mobile || (!user.mobile && user.role == "user");
+  if (incomplete) {
+    return <EditRoleMobile />;
+  }
   return <div></div>;
 }
 
-export default page;
+export default Home;
