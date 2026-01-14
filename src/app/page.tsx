62f6497a183777a208125890c3653e2797a01c3e
Samiksha Jain <samikshaj7371@gmail.com>
2026-01-14 22:32:18 +0530

    create Nav component for user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import { auth } from "@/auth";
+import AdminDashboard from "@/components/AdminDashboard";
+import DeliveryBoy from "@/components/DeliveryBoy";
 import EditRoleMobile from "@/components/EditRoleMobile";
+import Nav from "@/components/Nav";
+import UserDashboard from "@/components/UserDashboard";
 import connectDb from "@/lib/db";
 import User from "@/models/user.model";
 import { redirect } from "next/navigation";
@@ -18,7 +22,20 @@ async function Home() {
   if (incomplete) {
     return <EditRoleMobile />;
   }
-  return <div></div>;
+
+  const plainUser = JSON.parse(JSON.stringify(user));
+  return (
+    <>
+      <Nav user={plainUser} />
+      {user.role == "user" ? (
+        <UserDashboard />
+      ) : user.role == "admin" ? (
+        <AdminDashboard />
+      ) : (
+        <DeliveryBoy />
+      )}
+    </>
+  );
 }
 
 export default Home;
---
+++ src/components/UserDashboard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UserDashboard() {
+  return <div></div>;
+}
+
+export default UserDashboard;
